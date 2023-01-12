428cb7e396d35e13f2783703c10ca864add5ea73
nomin.oyu <nomin.oyu@corp.unitel.mn>
2023-01-12 22:43:10 +0800

    login

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
 import { MENUS, users } from "./util/data";
+import { useState } from "react";
 
 function App() {
-  function LoginCheck(userName, password) {}
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  function LoginCheck(userName, password) {
+   
+      if (users.map((user) => user.userName === userName && user.password === password)) {
+        console.log("u are logged in")
+        setIsLoggedIn(true)
+      } else {
+        console.log("error")
+      }
+  }
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path={MENUS[0].url} element={<Home />} />
         <Route path={MENUS[1].url} element={<About />} />
-        <Route path={MENUS[2].url} element={<Login />} />
+        <Route path={MENUS[2].url} element={isLoggedIn ? <About /> : <Login logState = {LoginCheck }/> } />
       </Routes>
       <Footer />
     </div>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import { MENUS } from "../util/data";
 //preventdefault ==refresh
 
-export default function Login() {
+export default function Login(prop) {
+  const { logState } = prop;
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+
   function loginHandler(e) {
     e.preventDefault();
     setUserName(e.target.userName.value);
     setPassword(e.target.password.value);
+    logState(userName,password)
+    console.log(userName)
+    // loginCheck(userName, password)
   }
   console.log("pass", password);
   console.log("user", userName);
